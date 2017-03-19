92bd1aa207cd5937c2af9377e0a7db3ba24da8e4
Roman Valiusenko <roman.valiusenko@gmail.com>
2017-03-19 22:23:52 +0000

    A new test for election safety check.

--- src/test/java/com/rvprg/raft/tests/functional/RaftElectionSafety.java
+++ src/test/java/com/rvprg/raft/tests/functional/RaftElectionSafety.java
@@ -17,18 +17,15 @@ import org.junit.Test;
 
 import com.rvprg.raft.protocol.Raft;
 import com.rvprg.raft.protocol.RaftObserver;
+import com.rvprg.raft.protocol.impl.LogEntry;
 import com.rvprg.raft.protocol.impl.RaftImpl;
 import com.rvprg.raft.protocol.impl.RaftObserverImpl;
 import com.rvprg.raft.tests.helpers.NetworkUtils;
 import com.rvprg.raft.transport.MemberId;
 
 public class RaftElectionSafety {
-    // TODO:
-    // Test:Check that the candidate with the more up to date log should win the
-    // election
-
     @Test(timeout = 60000)
-    public void testElectionSafetyProperty()
+    public void testElectionSafetyProperty_OneLeaderPerTerm()
             throws InterruptedException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         // This test creates a cluster containing clusterSize members. It then
         // starts all of them, and waits until a leader is elected. Then it
@@ -102,6 +99,70 @@ public class RaftElectionSafety {
             assertEquals(1, termsAndLeaders.get(term).intValue());
             prevTerm = term;
         }
+    }
+
+    @Test(timeout = 60000)
+    public void testElectionSafetyProperty_MostUpToDateLogWins()
+            throws InterruptedException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        // This test creates a cluster containing clusterSize members. It then
+        // creates a few entries in the log of the majority of the
+        // candidates. Then it starts the election process and checks if a
+        // leader is from that majority.
+        int clusterSize = 5;
+
+        List<Raft> rafts = new ArrayList<Raft>();
+        Set<MemberId> members = new HashSet<MemberId>();
+        for (Integer port : NetworkUtils.getRandomFreePorts(clusterSize)) {
+            members.add(new MemberId("localhost", port));
+        }
+
+        CountDownLatch startLatch = new CountDownLatch(members.size());
+        final SynchronousQueue<Raft> leaderBlockingQueue = new SynchronousQueue<>();
+
+        RaftObserver observer = new RaftObserverImpl() {
+            @Override
+            public synchronized void electionWon(int term, Raft leader) {
+                try {
+                    leaderBlockingQueue.put(leader);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void started() {
+                startLatch.countDown();
+            }
+        };
+
+        for (MemberId member : members) {
+            Set<MemberId> peers = (new HashSet<MemberId>(members));
+            peers.remove(member);
+            rafts.add(RaftTestUtils.getRaft(member.getHostName(), member.getPort(), peers, observer));
+        }
+
+        Set<Raft> majority = new HashSet<Raft>();
+
+        for (int i = 0; i < clusterSize / 2 + 1; ++i) {
+            Raft raft = rafts.get(i);
+            majority.add(raft);
+            raft.getLog().append(new LogEntry(1, new byte[] { 2 }));
+        }
+
+        for (Raft raft : rafts) {
+            raft.start();
+        }
+
+        startLatch.await();
+
+        Raft currentLeader = leaderBlockingQueue.take();
+
+        assertTrue(majority.contains(currentLeader));
+
+        for (Raft raft : rafts) {
+            raft.shutdown();
+        }
+
     }
 
 }
